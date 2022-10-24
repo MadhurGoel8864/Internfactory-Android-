bee8eb0eb804900c65fc0a4329e11e14ef7f378e
Naman Bansal <bansalnaman44@gmail.com>
2022-10-25 01:21:26 +0530

    initial commit

--- app/src/main/java/com/example/internfactory/internfactory_main.kt
+++ app/src/main/java/com/example/internfactory/internfactory_main.kt
@@ -1,11 +1,17 @@
 package com.example.internfactory
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : Activity() {
+    fun signUpActivity(view: View){
+        val intent :Intent= Intent(this,sign_up_page::class.java)
+        startActivity(intent)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up_page)
+        setContentView(R.layout.login_pag_1)
     }
 }
