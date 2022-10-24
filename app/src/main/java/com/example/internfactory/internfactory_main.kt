396199024358318c45c648edb620a545b4251487
Madhur <madhur12a16072@gmail.com>
2022-10-24 12:10:33 +0530

    Login and Signin pages Made

---
+++ app/src/main/java/com/example/internfactory/internfactory_main.kt
@@ -0,0 +1,11 @@
+package com.example.internfactory
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_page)
+    }
+}
---
+++ app/src/main/java/com/example/internfactory/sign_in_page.kt
@@ -0,0 +1,12 @@
+package com.example.internfactory
+
+import android.app.Activity
+import android.os.Bundle
+
+class sign_in_page : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in_page)
+    }
+}
+
