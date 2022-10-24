2014d738fe3ef6dd6dad8e53722bf1e79cd8370d
Madhur <madhur12a16072@gmail.com>
2022-10-25 03:12:51 +0530

    Pages Styles Changed and Activities linking added

--- app/src/main/java/com/example/internfactory/sign_up_page.kt
+++ app/src/main/java/com/example/internfactory/sign_up_page.kt
@@ -1,11 +1,18 @@
 package com.example.internfactory
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class sign_up_page : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_page)
+        val sign_in_activity_btn = findViewById<Button>(R.id.login_btn)
+        sign_in_activity_btn.setOnClickListener {
+            val Intent = Intent(this,sign_in_page::class.java)
+            startActivity(Intent)
+        }
     }
 }
