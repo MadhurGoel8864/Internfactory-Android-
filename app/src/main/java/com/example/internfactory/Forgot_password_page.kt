a97ecc6b32c7b3b654cb3f4b840ce15a0a25c8d7
Madhur <madhur12a16072@gmail.com>
2022-10-25 12:18:26 +0530

    Verifiaction page Linked

--- app/src/main/java/com/example/internfactory/Forgot_password_page.kt
+++ app/src/main/java/com/example/internfactory/Forgot_password_page.kt
@@ -1,12 +1,21 @@
 package com.example.internfactory
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Forgot_password_page : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password_page)
+
+        val send_btn = findViewById<Button>(R.id.otp_btn)
+        send_btn.setOnClickListener {
+            val Intent = Intent(this,verification_page::class.java)
+            startActivity(Intent)
+        }
+
     }
 }
