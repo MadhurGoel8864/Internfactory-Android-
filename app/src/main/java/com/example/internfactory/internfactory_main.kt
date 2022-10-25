78e32c4940dcd8742f94a609616dd4af58faa09a
Madhur <madhur12a16072@gmail.com>
2022-10-25 11:19:34 +0530

    Splash Screen Bugs Fixed

--- app/src/main/java/com/example/internfactory/internfactory_main.kt
+++ app/src/main/java/com/example/internfactory/internfactory_main.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
-
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +13,8 @@ class MainActivity : Activity() {
         Handler().postDelayed({
                               val Intent = Intent(this@MainActivity,Home_page::class.java)
             startActivity(Intent)
+            finish()
         },3000)
-
 //        val sign_in_activity_btn = findViewById<Button>(R.id.login_btn)
 //        val sign_up_activity_btn = findViewById<Button>(R.id.sign_up_btn)
 //        sign_in_activity_btn.setOnClickListener {
