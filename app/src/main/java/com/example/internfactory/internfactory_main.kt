8180a0004b6b8f132ea20366fc5c6995bc0a49cb
Madhur <madhur12a16072@gmail.com>
2022-10-25 11:06:43 +0530

    Splash Screen added

--- app/src/main/java/com/example/internfactory/internfactory_main.kt
+++ app/src/main/java/com/example/internfactory/internfactory_main.kt
@@ -3,22 +3,29 @@ package com.example.internfactory
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_pag_1)
-        val sign_in_activity_btn = findViewById<Button>(R.id.login_btn)
-        val sign_up_activity_btn = findViewById<Button>(R.id.sign_up_btn)
-        sign_in_activity_btn.setOnClickListener {
-            val Intent = Intent(this, sign_in_page::class.java)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+                              val Intent = Intent(this@MainActivity,Home_page::class.java)
             startActivity(Intent)
-        }
-        sign_up_activity_btn.setOnClickListener {
-            val Intent = Intent(this,sign_up_page::class.java)
-            startActivity(Intent)
-        }
+        },3000)
+
+//        val sign_in_activity_btn = findViewById<Button>(R.id.login_btn)
+//        val sign_up_activity_btn = findViewById<Button>(R.id.sign_up_btn)
+//        sign_in_activity_btn.setOnClickListener {
+//            val Intent = Intent(this, sign_in_page::class.java)
+//            startActivity(Intent)
+//        }
+//        sign_up_activity_btn.setOnClickListener {
+//            val Intent = Intent(this,sign_up_page::class.java)
+//            startActivity(Intent)
+//        }
 
 
 
