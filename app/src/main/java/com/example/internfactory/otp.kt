a113ee5cd0d60ca069f3bc2c707e819c7e02b305
Naman Bansal <bansalnaman44@gmail.com>
2022-10-25 11:08:30 +0530

    Otp page added

---
+++ app/src/main/java/com/example/internfactory/otp.kt
@@ -0,0 +1,11 @@
+package com.example.internfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class otp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
