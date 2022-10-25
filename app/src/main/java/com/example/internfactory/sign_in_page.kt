4fff17eef444924e5926d6cf1d01158839e8d4f5
Madhur <madhur12a16072@gmail.com>
2022-10-26 01:52:42 +0530

    Validation added

--- app/src/main/java/com/example/internfactory/sign_in_page.kt
+++ app/src/main/java/com/example/internfactory/sign_in_page.kt
@@ -10,36 +10,52 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
-
+import com.google.android.material.textfield.TextInputLayout
+//and (password_inp.text?.isNotEmpty() == true)
 class sign_in_page : Activity() {
+
+    private lateinit var password_text : TextInputEditText
     private lateinit var ed1 : EditText
     private lateinit var login_btn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in_page)
         ed1 = findViewById(R.id.ed1)
+        login_btn  =findViewById(R.id.login_btn)
+        password_text = findViewById(R.id.password_input)
+
         ed1.addTextChangedListener(object:TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
             }
-
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
-
             override fun afterTextChanged(p0: Editable?) {
-                if (android.util.Patterns.EMAIL_ADDRESS.matcher(ed1.text.toString()).matches() )
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(ed1.text.toString()).matches() and (password_text.text?.isNotEmpty() == true)){
                     login_btn.isEnabled = true
+                }
                 else {
                     login_btn.isEnabled = false
-                    ed1.setError("Invalid Email Format ")
+//                    ed1.setError("Invalid Email Format ")
+                }
+            }
+        })
+        password_text.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(ed1.text.toString()).matches() and (password_text.text?.isNotEmpty() == true)){
+                    login_btn.isEnabled = true
+                }
+                else {
+                    login_btn.isEnabled = false
+//                    ed1.setError("Invalid Email Format ")
                 }
             }
 
-        })
-//        login_btn = findViewById(R.id.login_btn)
-//
-//        login_btn.setOnClickListener {  }
 
+        })
         val login_button = findViewById<Button>(R.id.login_btn)
         login_button.setOnClickListener {
             val Intent = Intent(this,sign_up_page::class.java)
--- app/src/main/java/com/example/internfactory/sign_up_page.kt
+++ app/src/main/java/com/example/internfactory/sign_up_page.kt
@@ -1,19 +1,97 @@
 package com.example.internfactory
-
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
+import android.view.inputmethod.InputBinding
 import android.widget.Button
 import android.widget.EditText
-
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 class sign_up_page : Activity() {
+    private lateinit var firstname : TextInputEditText
+    private lateinit var lastname : TextInputEditText
+    private lateinit var email : TextInputEditText
+    private lateinit var password : TextInputEditText
+    private lateinit var button : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_page)
+        button = findViewById(R.id.sign_up_btn)
+        firstname = findViewById(R.id.first_name_inp)
+        lastname = findViewById(R.id.last_name_inp)
+        email= findViewById(R.id.email_inp)
+        password= findViewById(R.id.password_form_inp)
+
+        firstname.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches() and (password.text?.isNotEmpty() == true) and (firstname.text?.isNotEmpty()==true) and (lastname.text?.isNotEmpty()==true) ){
+                    button.isEnabled = true
+                }
+                else {
+                    button.isEnabled = false
+//                    ed1.setError("Invalid Email Format ")
+                }
+            }
+        })
+        lastname.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches() and (password.text?.isNotEmpty() == true) and (firstname.text?.isNotEmpty()==true) and (lastname.text?.isNotEmpty()==true) ){
+                    button.isEnabled = true
+                }
+                else {
+                    button.isEnabled = false
+//                    ed1.setError("Invalid Email Format ")
+                }
+            }
+        })
+
+        email.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches() and (password.text?.isNotEmpty() == true) and (firstname.text?.isNotEmpty()==true) and (lastname.text?.isNotEmpty()==true) ){
+                    button.isEnabled = true
+                }
+                else {
+                    button.isEnabled = false
+//                    ed1.setError("Invalid Email Format ")
+                }
+            }
+        })
+
+        password.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches() and (password.text?.isNotEmpty() == true) and (firstname.text?.isNotEmpty()==true) and (lastname.text?.isNotEmpty()==true) ){
+                    button.isEnabled = true
+                }
+                else {
+                    button.isEnabled = false
+//                    ed1.setError("Invalid Email Format ")
+                }
+            }
+        })
+
+
+
+
 
         val sign_in_activity_btn = findViewById<Button>(R.id.login_btn)
         sign_in_activity_btn.setOnClickListener {
