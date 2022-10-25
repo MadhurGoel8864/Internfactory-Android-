4506c393b2f091ab5f772ab9787abb2745defa75
Naman Bansal <bansalnaman44@gmail.com>
2022-10-25 11:31:57 +0530

    otp verification

--- app/src/main/java/com/example/internfactory/otp.kt
+++ app/src/main/java/com/example/internfactory/otp.kt
@@ -1,9 +1,10 @@
 package com.example.internfactory
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class otp : AppCompatActivity() {
+class otp : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp)
