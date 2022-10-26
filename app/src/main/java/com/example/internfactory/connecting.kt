10463b250ea705b4b3461f68a5bb5f13038c4abb
Naman Bansal <bansalnaman44@gmail.com>
2022-10-26 17:55:53 +0530

    files added

--- app/src/main/java/com/example/internfactory/connecting.kt
+++ app/src/main/java/com/example/internfactory/connecting.kt
@@ -8,6 +8,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import kotlin.math.log
 
 class connecting : AppCompatActivity() {
 
