9a9989ceb415d5c95bcc7e9cefba1b97a434755b
Madhur <madhur12a16072@gmail.com>
2022-10-29 00:57:42 +0530

    Sign Up Validation Change

--- app/src/main/java/com/example/internfactory/Activities/SignIn_Fragment.kt
+++ app/src/main/java/com/example/internfactory/Activities/SignIn_Fragment.kt
@@ -1,18 +1,23 @@
 package com.example.internfactory.Activities
-
+import android.app.AlertDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.widget.addTextChangedListener
 import com.example.internfactory.R
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 class SignIn_Fragment : Fragment() {
     private lateinit var password_text : TextInputEditText
+    private lateinit var password_cont : TextInputLayout
     private lateinit var ed1 : TextInputEditText
+    private lateinit var email_cont : TextInputLayout
     private lateinit var login_btn : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,34 +25,45 @@ class SignIn_Fragment : Fragment() {
     ): View? {        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_sign_in_, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         password_text = requireView().findViewById(R.id.password_input)
         ed1 = requireView().findViewById(R.id.email_inp)
         login_btn = requireView().findViewById(R.id.login_btn)
-        ed1.addTextChangedListener(object: TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun afterTextChanged(p0: Editable?) {
-                login_btn.isEnabled =
-                    android.util.Patterns.EMAIL_ADDRESS.matcher(ed1.text.toString()).matches() and (password_text.text?.isNotEmpty() == true)
-            }
-        })
-        password_text.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun afterTextChanged(p0: Editable?) {
-                login_btn.isEnabled =
-                    android.util.Patterns.EMAIL_ADDRESS.matcher(ed1.text.toString()).matches() and (password_text.text?.isNotEmpty() == true)
-            }
-        })
-
+        email_cont = requireView().findViewById(R.id.ed1)
+        password_cont = requireView().findViewById(R.id.password_inp)
+        ed1.addTextChangedListener{
+            login_btn.isEnabled = (email_cont.helperText == null) and (password_cont.helperText == null)
+                    email_cont.helperText = validemail()
+        }
+        password_text.addTextChangedListener{
+            login_btn.isEnabled = (email_cont.helperText == null) and (password_cont.helperText == null)
+            password_cont.helperText = validemail()
+        }
     }
 
-}
+    private fun validPass(): String? {
+        val pass_txt = password_text.text.toString()
+        if(pass_txt.length<8){
+            return "Minimum 8 characters Required"
+        }
+        if(!pass_txt.matches(".*[A-Z]*.".toRegex())){
+            return "At least 1 UpperCase Alphabet Required"
+        }
+        if(!pass_txt.matches(".*[a-z]*.".toRegex())){
+            return "At least 1 LowerCase Alphabet Required"
+        }
 
+        if(!pass_txt.matches(".*[@#$%^&*+=]*.".toRegex())){
+            return "At least 1  Special Character Required"
+        }
+    return null
+    }
+    private fun validemail(): String? {
+        val email_text = ed1.text.toString()
+        if(!Patterns.EMAIL_ADDRESS.matcher(email_text).matches()){
+            return "Invalid Email Address"
+        }
+        return null
+    }
+}
