58cb1eceb770c5415e1ab75c6c5fa1c11387b5a8
Naman Bansal <bansalnaman44@gmail.com>
2022-10-28 20:55:40 +0530

    button disable property

--- app/src/main/java/com/example/internfactory/modules/User.kt
+++ app/src/main/java/com/example/internfactory/modules/User.kt
@@ -3,10 +3,8 @@ package com.example.internfactory.modules
 import com.google.gson.annotations.SerializedName
 
 data class User(
-    @SerializedName("id") val id:Int?,
-    @SerializedName("email") val email : String?,
     @SerializedName("firstName") val firstName : String?,
     @SerializedName("lastName") val lastName : String?,
-    @SerializedName("password") val password : String?,
-    @SerializedName("role") val role:String?
-)
+    @SerializedName("email") val email : String?,
+    @SerializedName("password") val password : String?
+    )
